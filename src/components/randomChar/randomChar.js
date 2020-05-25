d4780566e72dad91a277979c87c791f3d40b9ffd
Strynzha Roman <johndoe@example.com>
2020-05-25 11:43:38 +0300

    random Char ErrorMessage & other

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './randomChar.css';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import GotService from '../../services/gotService';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 export default class RandomChar extends Component {
 
@@ -13,46 +15,74 @@ export default class RandomChar extends Component {
     gotService = new GotService();
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
     }
 
     onCharLoaded = (char) => {
-        this.setState({ char });
+        this.setState({
+            char,
+            loading: false
+        });
     }
 
     updateChar() {
-        const id = Math.floor(Math.random() * 140 + 25);
+        const id = 13000000;
+        // const id = Math.floor(Math.random() * 140 + 25);
 
         this.gotService.getCharacter(id)
-            .then(this.onCharLoaded);
+            .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     render() {
 
-        const { char: { name, gender, born, died, culture } } = this.state;
+        const { char, loading, error } = this.state;
+
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: {name}</h4>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem className="d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>{gender}</span>
-                    </ListGroupItem>
-                    <ListGroupItem className="d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>{born}</span>
-                    </ListGroupItem>
-                    <ListGroupItem className="d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>{died}</span>
-                    </ListGroupItem>
-                    <ListGroupItem className="d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>{culture}</span>
-                    </ListGroupItem>
-                </ListGroup>
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         );
     }
+}
+
+const View = ({ char: { name, gender, born, died, culture } }) => {
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem className="d-flex justify-content-between">
+                    <span className="term">Gender </span>
+                    <span>{gender}</span>
+                </ListGroupItem>
+                <ListGroupItem className="d-flex justify-content-between">
+                    <span className="term">Born </span>
+                    <span>{born}</span>
+                </ListGroupItem>
+                <ListGroupItem className="d-flex justify-content-between">
+                    <span className="term">Died </span>
+                    <span>{died}</span>
+                </ListGroupItem>
+                <ListGroupItem className="d-flex justify-content-between">
+                    <span className="term">Culture </span>
+                    <span>{culture}</span>
+                </ListGroupItem>
+            </ListGroup>
+        </>
+    )
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+
+    return (
+        <div className="loadingio-spinner-spinner-8c696nslt9r">
+            <div className="ldio-cziz452eqrc">
+                <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Spinner;
